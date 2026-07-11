c9792107f59609bb4f7ee4f8a01021c95ad9822f
komndanismail-hue <komndanismail@gmail.com>
2026-07-11 15:13:54 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = "mraco-v1";
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./", "index.html", "manifest.json"])));
+  self.skipWaiting();
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener("fetch", (e) => {
+  if (e.request.mode === "navigate") {
+    e.respondWith(fetch(e.request).catch(() => caches.match("index.html")));
+  }
+});
